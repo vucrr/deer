a91ba201305b65183cc8d84cf0f5ddedce81c7b9
胡星球 <Vucrr.Hu@verystar.cn>
2023-08-04 15:33:17 +0800

    添加分享功能

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,2 +1,21 @@
 // index.js
-Page({})
+Page({
+  onShareAppMessage() {
+    const promise = new Promise(resolve => {
+      setTimeout(() => {
+        resolve({
+          title: '和小鹿的游记'
+        })
+      }, 2000)
+    })
+    return {
+      title: '和小鹿的游记',
+      path: 'pages/home/index',
+    }
+  },
+  onShareTimeline() {
+    return {
+      title: '和小鹿的游记'
+    }
+  }
+})
